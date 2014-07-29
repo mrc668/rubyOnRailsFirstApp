47368a182ac0d8222ac46d1273ec9ebaba6cb58c
Milton Calnek <milton@calnek.com>
2014-07-28 21:59:51 -0600

    Initial repository of first app on mrcDeskTop.eotn.calnek.com

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '77a8ce8460ccadb6398289fdc1f2191622735bc85ce9ac8fefc12a3232789b3bf47fb2ef9bfc4fdd2fe60c222804d231c2b45de9ae3bd35e481b0b5d2fcdc260'
